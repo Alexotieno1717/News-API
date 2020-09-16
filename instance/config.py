81ff1224862fdc74cbca6bf4a381916be382df83
Alexotieno1717 <alexotieno900@gmail.com>
2020-09-16 18:14:18 +0300

    Deployment

--- instance/config.py
+++ instance/config.py
@@ -3,6 +3,8 @@ class Config:
     General configuration parent class
     """
     pass
+    api_key = 'a493e30f11b147d0ba67b15ca60c5e4c'
+    SECRET_KEY = '1234567890'
 
 
 class ProdConfig(Config):
